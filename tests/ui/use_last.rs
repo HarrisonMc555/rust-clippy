98ecce74e85854bd0fc96362986e762b7cc9c5b3
Harrison McCullough <mccullough.harrison@gmail.com>
2019-02-23 21:53:35 -0700

    Remove author_lint from ui test

--- tests/ui/use_last.rs
+++ tests/ui/use_last.rs
@@ -3,7 +3,6 @@
 
 fn dont_use_last() -> Option<i32> {
     let x = vec![2, 3, 5];
-    #[clippy::author]
     let last_element = x.get(x.len() - 1); // ~ERROR Use _.last()
     last_element.map(|val| val + 1) // To avoid warnings
 }
@@ -15,7 +14,6 @@ fn index_into_last() -> i32 {
 }
 
 fn main() {
-    // let expected_value: &i32 = &5;
     let expected_value: i32 = 5;
     assert_eq!(dont_use_last(), Some(expected_value));
     assert_eq!(index_into_last(), 5);
