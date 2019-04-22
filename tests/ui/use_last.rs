6a84ef87860476ada25ea21791a6a20fdcf1d14f
Harrison McCullough <mccullough.harrison@gmail.com>
2019-04-22 10:00:57 -0600

    Update ui test and stderr

--- tests/ui/use_last.rs
+++ tests/ui/use_last.rs
@@ -1,36 +1,29 @@
 #![warn(clippy::use_last)]
 
-fn dont_use_last() -> Option<i32> {
+fn dont_use_last() {
     let x = vec![2, 3, 5];
-    let last_element = x.get(x.len() - 1); // ~ERROR Use x.last()
-    last_element.map(|val| val + 1) // To avoid warnings
+    let _ = x.get(x.len() - 1); // ~ERROR Use x.last()
 }
 
-fn indexing_two_from_end() -> Option<i32> {
+fn indexing_two_from_end() {
     let x = vec![2, 3, 5];
-    let last_element = x.get(x.len() - 2);
-    last_element.map(|val| val + 3) // To avoid warnings
+    let _ = x.get(x.len() - 2);
 }
 
-fn index_into_last() -> i32 {
+fn index_into_last() {
     let x = vec![2, 3, 5];
-    let last_element = x[x.len() - 1];
-    last_element + 1 // To avoid warnings
+    let _ = x[x.len() - 1];
 }
 
-// False positive test (currently failing)
-fn use_last_with_different_vec_length() -> Option<i32> {
+fn use_last_with_different_vec_length() {
     let x = vec![2, 3, 5];
     let y = vec!['a', 'b', 'c'];
-    let last_element = x.get(y.len() - 1);
-    last_element.map(|val| val + 1)
+    let _ = x.get(y.len() - 1);
 }
 
 fn main() {
-    let expected_value: i32 = 6;
-    println!("Working...");
-    assert_eq!(dont_use_last(), Some(expected_value));
-    assert_eq!(indexing_two_from_end(), Some(expected_value));
-    assert_eq!(index_into_last(), expected_value);
-    assert_eq!(use_last_with_different_vec_length(), Some(expected_value));
+    dont_use_last();
+    indexing_two_from_end();
+    index_into_last();
+    use_last_with_different_vec_length();
 }
